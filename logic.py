beb538a0c138ffd308c89e4bc888a12d9a2bbd45
Sebi <mihaisebastian01@gmail.com>
2021-07-25 19:56:24 +0300

    something

---
+++ logic.py
@@ -0,0 +1,7 @@
+"""
+> world’s simplest face recognition library.
+> Dlib is a modern C++ toolkit containing machine learning algorithms and tools for creating complex software in C++
+to solve real world problems.
+> Built using dlib’s state-of-the-art face recognition built with deep learning.
+The model has an accuracy of 99.38% on the Labeled Faces in the Wild benchmark.
+"""
